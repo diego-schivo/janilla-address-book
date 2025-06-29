665d55f8f7b838c13e53aa375aa17c2f95e31f45
Diego Schivo <diego.schivo@gmail.com>
2025-06-29 13:41:40 +0200

    sleep

--- source/com/janilla/addressbook/CustomMethodHandlerFactory.java
+++ source/com/janilla/addressbook/CustomMethodHandlerFactory.java
@@ -42,12 +42,12 @@ public class CustomMethodHandlerFactory extends MethodHandlerFactory {
 				throw new HandleException(new MethodBlockedException());
 		}
 
-		if (exchange.getRequest().getPath().startsWith("/api/"))
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+//		if (exchange.getRequest().getPath().startsWith("/api/"))
+//			try {
+//				Thread.sleep(500);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
 
 		super.handle(invocation, exchange);
 	}
